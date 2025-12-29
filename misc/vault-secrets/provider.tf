edcc502b36a8b764e2b7366d2b0cecd18eff6976
Aarti chaple <aartichaple2124@gmail.com>
2025-12-30 00:17:37 +0530

    test

--- misc/vault-secrets/provider.tf
+++ misc/vault-secrets/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "RG"
     storage_account_name = "rttfstatestorage"
-    container_name       = "tfstate"
+    container_name       = "vault-state"
     key                  = "dev.tfstate"
 
   }
